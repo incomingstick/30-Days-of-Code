d4f0f4538e8d87f704966ccdb5116cfb98b45557
incomingstick <incomingstick@gmail.com>
2016-08-02 12:01:50 -0500

    Day 7 Answer

---
+++ Day 7/day7.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    int n; 
+    scanf("%d",&n);
+
+    int *arr = malloc(sizeof(int) * n);
+
+    for(int arr_i = 0; arr_i < n; arr_i++) {
+        scanf("%d", &arr[arr_i]);
+    }
+
+    for(int arr_i = n; arr_i > 0; arr_i--) {
+        printf("%d ", arr[arr_i - 1]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
